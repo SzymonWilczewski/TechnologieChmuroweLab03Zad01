e7a3cba391c8862af0376e30c2527b09e4b0c7c6
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2021-03-15 22:33:23 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("Hello from my node app!");
+});
+
+app.listen(port, () => {
+  console.log(`API server listening at port ${port}`);
+});
